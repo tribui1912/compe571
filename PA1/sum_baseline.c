a783fa110c0dc58ffd1466f41a90b3f3864a7c55
Tri Bui <tribui1912@gmail.com>
2024-09-12 18:03:48 -0700

    add test_baseline

--- PA1/sum_baseline.c
+++ PA1/sum_baseline.c
@@ -3,11 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc != 2)
-  {
-    fprintf(stderr, "Usage %s <N>\n", argv[0]);
-    return 1;
-  }
   int N = atoi(argv[1]);
   long long sum = 0;
   for (int i = 0; i < N; i++)
---
+++ PA1/test_baseline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#compile sum_baseline.c
+gcc -o sum_baseline sum_baseline.c
+
+#test value
+values=(100000000 1000000000 10000000000)
+
+#run test
+for value in "${values[@]}"
+do 
+  echo "Testing with N = $value"
+  time ./sum_baseline.o $value
+  echo "-----------------------"
+done
+
+
